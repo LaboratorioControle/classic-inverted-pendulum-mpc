d03cd6fb5ba142a54d6bf4c25e7e972b7d6366e6
Mateus Henrique Teixeira <mateushtteixeira@gmail.com>
2026-02-14 16:57:37 -0300

    Adequação para leitura dos dados no csv e simulação da senoide

--- simulation/sim_pendulum_senoid.m
+++ simulation/sim_pendulum_senoid.m
@@ -9,20 +9,24 @@
 
 
 %% Inicialização
+
+clear;
 close all;                       % Fecha todas as figuras abertas
 
+run init_project;
+clc;
 %% CONFIGURAÇÕES DE PLOT (DINÂMICO)
 
 plot_discreto     = true;   % Modelo discreto (RK4)
-plot_continuo     = true;    % Modelo contínuo (ODE45)
+plot_continuo     = false;    % Modelo contínuo (ODE45)
 plot_experimental = true;   % Dados experimentais (se houver)
 
 %% PARÂMETROS DA SIMULAÇÃO - ENTRADA SENOIDAL
 
-t_final   = 60;                    % Tempo total de simulação [s]
+t_final   = 90;                    % Tempo total de simulação [s]
 tau       = dados.geral.Ts;        % Tempo de amostragem [s]
 amplitude = 200*12/255;            % Amplitude da senoide [V]
-duracao   = 5000/1000;             % Duração do sinal [s]
+duracao   = 2500/1000;             % Duração do sinal [s]
 frequencia = 2;                    % Frequência da senoide [Hz]
 
 % Condições iniciais
@@ -49,7 +53,8 @@ end
 
 % Conversão de unidades
 x(:,1) = x(:,1) * 100;         % m → cm
-x(:,2) = x(:,2) * (180/pi);    % rad → °
+%x(:,2) = x(:,2) * (180/pi);    % rad → °
+x(:,2) = wrapTo360((180/pi) * x(:,2));
 x(:,3) = x(:,3) * 100;         % m/s → cm/s
 x(:,4) = x(:,4) * (180/pi);    % rad/s → °/s
 
@@ -65,20 +70,28 @@ tspan = [0 t_final];
 
 % Conversão de unidades
 x2(:,1) = x2(:,1) * 100;
-x2(:,2) = x2(:,2) * (180/pi);
+%x2(:,2) = x2(:,2) * (180/pi);
+x2(:,2) = wrapTo360((180/pi) * x2(:,2));
+
 x2(:,3) = x2(:,3) * 100;
 x2(:,4) = x2(:,4) * (180/pi);
 
 %% DADOS EXPERIMENTAIS (OPCIONAL)
 
-importados = importdata('data\raw\dados_senoide_I200_D5000_F2_174837.csv');
-t_import = importados(:,1) - importados(1,1);
-x_import = importados(:,2:5);
+importados = importdata('data\raw\dados_senoide_I200_F2_D2500.csv');
 
-posicao_import     = x_import(:,3) * 100;
-angulo_import      = x_import(:,1);
-velocidade_import  = x_import(:,4) * 100;
-vel_angular_import = x_import(:,2);
+off_set = 125;
+
+t_import = importados(:,1) - importados(off_set,1);
+t_import = t_import/1000;
+t_import = t_import(off_set:end);
+
+angulo_import      = importados(off_set:end,2);
+vel_angular_import = importados(off_set:end,3);
+posicao_import     = importados(off_set:end,4);
+velocidade_import  = importados(off_set:end,5);
+u_import           = importados(off_set:end,6);
+
 
 %% PLOT DOS RESULTADOS (DINÂMICO)
 
@@ -100,10 +113,10 @@ if plot_experimental
      stairs(t_import, posicao_import);
 end
 if plot_continuo
-    plot(t2, x2(:,1), 'LineWidth', 1.5);
+    plot(t2, x2(:,1));
 end
 if plot_discreto
-    stairs(t, x(:,1), '--');
+    stairs(t, x(:,1));
 end
 legend(legend_entries);
 
@@ -117,10 +130,10 @@ if plot_experimental
     stairs(t_import, angulo_import);
 end
 if plot_continuo
-    plot(t2, x2(:,2), 'LineWidth', 1.5);
+    plot(t2, x2(:,2));
 end
 if plot_discreto
-    stairs(t, x(:,2), '--');
+    stairs(t, x(:,2));
 end
 legend(legend_entries);
 
@@ -134,10 +147,10 @@ if plot_experimental
     stairs(t_import, velocidade_import);
 end
 if plot_continuo
-    plot(t2, x2(:,3), 'LineWidth', 1.5);
+    plot(t2, x2(:,3));
 end
 if plot_discreto
-    stairs(t, x(:,3), '--');
+    stairs(t, x(:,3));
 end
 legend(legend_entries);
 
@@ -151,15 +164,15 @@ if plot_experimental
     stairs(t_import, vel_angular_import);
 end
 if plot_continuo
-    plot(t2, x2(:,4), 'LineWidth', 1.5);
+    plot(t2, x2(:,4));
 end
 if plot_discreto
-    stairs(t, x(:,4), '--');
+    stairs(t, x(:,4));
 end
 legend(legend_entries);
 
 
 %% LIMPEZA
 
-clear k tspan t_final tau amplitude duracao frequencia angulo_import idx importados legend_entries plot_discreto plot_continuo plot_experimental;
-clear posicao_import t t2 t_import u vel_angular_import velocidade_import x x0 x2 x_import;
+clear k tspan t_final tau idx importados legend_entries plot_discreto plot_continuo plot_experimental;
+clear t t2 u off_set;
