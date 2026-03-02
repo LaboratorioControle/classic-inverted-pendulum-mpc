ed35a36c27ec9359eaceb6322f56cd2bf69dcb94
Mateus Henrique Teixeira <mateushtteixeira@gmail.com>
2026-03-02 18:00:00 -0300

    Novos parâmetros do modelo (Ajustado com base na senoide e no SwingUp)

--- params/params_pendulum.m
+++ params/params_pendulum.m
@@ -8,9 +8,9 @@ dados.geral.g = 9.81;          % [m/s^2] Aceleração da gravidade
 % ==============================
 % Dados do pêndulo
 % ==============================
-dados.pendulo.m = 20.4/1000;        % [kg]    Massa da haste do pêndulo
-dados.pendulo.l = 0.18;        % [m]     Distância do ponto de fixação até o centro de gravidade
-dados.pendulo.I = 0.0003;  % [kg·m^2] Momento de inércia da haste em relação ao centro
+dados.pendulo.m = 66.9/1000;        % [kg]    Massa da haste do pêndulo
+dados.pendulo.l = 0.15;        % [m]     Distância do ponto de fixação até o centro de gravidade
+dados.pendulo.I = 0.0013;  % [kg·m^2] Momento de inércia da haste em relação ao centro
 dados.pendulo.b = 0;   % [N·m·s] Coeficiente de amortecimento viscoso no eixo de fixação
 dados.geral.guia = 0.60; % [m] Tamanho da guia linear
 
@@ -18,17 +18,17 @@ dados.geral.guia = 0.60; % [m] Tamanho da guia linear
 % ==============================
 % Dados do carro
 % ==============================
-dados.carro.m = 350.5/1000;         % [kg]    Massa do carro
-dados.carro.c = 11.2498;          % [N·s/m] Coeficiente de amortecimento viscoso entre carro e guia
+dados.carro.m = 372.5/1000;         % [kg]    Massa do carro
+dados.carro.c = 11.1755;          % [N·s/m] Coeficiente de amortecimento viscoso entre carro e guia
 
 
 % ==============================
 % Dados do motor
 % ==============================
-dados.motor.Rm = 6.8845;         % [Ω]      Resistência do enrolamento (Chute)
-dados.motor.Kb = 0.0177;         % [V·s/rad] Constante de força contra-eletromotriz (Chute)
-dados.motor.Kt = 0.1748;         % [N·m/A]  Constante de torque (Torque/Corrente)
-dados.motor.R  = 0.071;         % [-]      Relação de transmissão (1/14)
+dados.motor.Rm = 11.8180;         % [Ω]      Resistência do enrolamento (Chute)
+dados.motor.Kb = 0.1115;         % [V·s/rad] Constante de força contra-eletromotriz (Chute)
+dados.motor.Kt = 0.1749;         % [N·m/A]  Constante de torque (Torque/Corrente)
+dados.motor.R  = 0.0389;         % [-]      Relação de transmissão (1/14)
 
 
 % ==============================
@@ -40,3 +40,4 @@ dados.carro.Fc = 0.035;      % N
 %% Geração das matrizes de estado
 
 run params_ss_pendulum;
+%run sim_pendulum_senoid;
