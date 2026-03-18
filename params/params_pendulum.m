fdcab7891d00996295b3c9a335f0741abbff7a75
Mateus Henrique Teixeira <mateushtteixeira@gmail.com>
2026-03-17 21:02:18 -0300

    Parâmetros ajustados para os prints

--- params/params_pendulum.m
+++ params/params_pendulum.m
@@ -8,9 +8,9 @@ dados.geral.g = 9.81;          % [m/s^2] Aceleração da gravidade
 % ==============================
 % Dados do pêndulo
 % ==============================
-dados.pendulo.m = 66.9/1000;        % [kg]    Massa da haste do pêndulo
+dados.pendulo.m = 69.1/1000;        % [kg]    Massa da haste do pêndulo
 dados.pendulo.l = 0.15;        % [m]     Distância do ponto de fixação até o centro de gravidade
-dados.pendulo.I = 0.0013;  % [kg·m^2] Momento de inércia da haste em relação ao centro
+dados.pendulo.I = 0.0012;  % [kg·m^2] Momento de inércia da haste em relação ao centro
 dados.pendulo.b = 0;   % [N·m·s] Coeficiente de amortecimento viscoso no eixo de fixação
 dados.geral.guia = 0.60; % [m] Tamanho da guia linear
 
@@ -18,17 +18,17 @@ dados.geral.guia = 0.60; % [m] Tamanho da guia linear
 % ==============================
 % Dados do carro
 % ==============================
-dados.carro.m = 372.5/1000;         % [kg]    Massa do carro
-dados.carro.c = 11.1755;          % [N·s/m] Coeficiente de amortecimento viscoso entre carro e guia
+dados.carro.m = 786.2/1000;         % [kg]    Massa do carro
+dados.carro.c = 8.4754;          % [N·s/m] Coeficiente de amortecimento viscoso entre carro e guia
 
 
 % ==============================
 % Dados do motor
 % ==============================
-dados.motor.Rm = 11.8180;         % [Ω]      Resistência do enrolamento (Chute)
-dados.motor.Kb = 0.1115;         % [V·s/rad] Constante de força contra-eletromotriz (Chute)
-dados.motor.Kt = 0.1749;         % [N·m/A]  Constante de torque (Torque/Corrente)
-dados.motor.R  = 0.0389;         % [-]      Relação de transmissão (1/14)
+dados.motor.Rm = 8.3490;         % [Ω]      Resistência do enrolamento (Chute)
+dados.motor.Kb = 1.0171;         % [V·s/rad] Constante de força contra-eletromotriz (Chute)
+dados.motor.Kt = 1.2110;         % [N·m/A]  Constante de torque (Torque/Corrente)
+dados.motor.R  = 0.4033;         % [-]      Relação de transmissão (1/14)
 
 
 % ==============================
